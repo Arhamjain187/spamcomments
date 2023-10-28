48a89fd8e68f8a804e1cb9e08daf07b311cdd1c3
Arhamjain187 <113501980+Arhamjain187@users.noreply.github.com>
2023-10-28 14:01:17 +0400

    Update content.js

--- content.js
+++ content.js
@@ -1,80 +1,96 @@
-// Retrieve the video ID from the URL
-const videoId = window.location.search.split('v=')[1];
+// Function to extract video ID from the URL
+function extractVideoIdFromUrl() {
+  const queryString = window.location.search;
+  const videoIdParam = 'v=';
+  const startIndex = queryString.indexOf(videoIdParam);
 
-// Define the YouTube Data API endpoint URL
-const youtubeApiUrl = `https://www.googleapis.com/youtube/v3/commentThreads`;
+  if (startIndex !== -1) {
+    const endIndex = queryString.indexOf('&', startIndex);
+    return endIndex === -1
+      ? queryString.substring(startIndex + videoIdParam.length)
+      : queryString.substring(startIndex + videoIdParam.length, endIndex);
+  }
 
-// Define the API key for YouTube Data API
-const youtubeApiKey = 'YOUR_YOUTUBE_API_KEY';
-
-// Define the Perspective API endpoint URL
-const perspectiveApiUrl = 'https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze';
-
-// Define the API key for Perspective API
-const perspectiveApiKey = 'YOUR_PERSPECTIVE_API_KEY';
+  return null;
+}
 
 // Function to fetch comments from the YouTube Data API
-function fetchComments(videoId) {
-  return fetch(`${youtubeApiUrl}?part=snippet&videoId=${videoId}&key=${youtubeApiKey}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to fetch comments from YouTube Data API');
-      }
-      return response.json();
-    })
-    .catch(error => {
-      console.error(error);
-    });
+async function fetchYouTubeComments(videoId) {
+  const apiUrl = `https://www.googleapis.com/youtube/v3/commentThreads`;
+  const apiKey = 'no_key'; // not uploaded for security reasons
+
+  try {
+    const response = await fetch(`${apiUrl}?part=snippet&videoId=${videoId}&key=${apiKey}`);
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch comments from YouTube Data API');
+    }
+
+    const data = await response.json();
+    return data.items || [];
+  } catch (error) {
+    console.error('Error fetching comments from YouTube Data API:', error);
+    return [];
+  }
 }
 
 // Function to check if a comment is spam using the Perspective API
-function checkForSpam(comment) {
-  return fetch(`${perspectiveApiUrl}?key=${perspectiveApiKey}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      comment: {
-        text: comment,
+async function checkCommentForSpam(comment) {
+  const apiUrl = 'https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze';
+  const apiKey = 'no_key'; // not uploaded for security reasons
+
+  try {
+    const response = await fetch(`${apiUrl}?key=${apiKey}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
       },
-      requestedAttributes: {
-        SPAM: {},
-      },
-    }),
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to analyze comment using Perspective API');
-      }
-      return response.json();
-    })
-    .then(data => data.attributeScores.SPAM.summaryScore.value)
-    .catch(error => {
-      console.error(error);
-      return 0; // Assume not spam on error
+      body: JSON.stringify({
+        comment: {
+          text: comment,
+        },
+        requestedAttributes: {
+          SPAM: {},
+        },
+      }),
     });
+
+    if (!response.ok) {
+      throw new Error('Failed to analyze comment using Perspective API');
+    }
+
+    const data = await response.json();
+    return data.attributeScores.SPAM.summaryScore.value || 0;
+  } catch (error) {
+    console.error('Error analyzing comment using Perspective API:', error);
+    return 0; // Assume not spam on error
+  }
 }
 
 // Function to process and flag spam comments
 async function processComments(videoId) {
-  try {
-    const commentData = await fetchComments(videoId);
+  const comments = await fetchYouTubeComments(videoId);
 
-    if (commentData.items) {
-      for (const item of commentData.items) {
-        const comment = item.snippet.topLevelComment.snippet.textDisplay;
-        const spamScore = await checkForSpam(comment);
+  for (const commentItem of comments) {
+    const commentText = commentItem.snippet.topLevelComment.snippet.textDisplay;
+    const spamScore = await checkCommentForSpam(commentText);
 
-        if (spamScore > 0.5) {
-          item.snippet.topLevelComment.snippet.textDisplay += ' (SPAM)';
-        }
-      }
+    if (spamScore > 0.5) {
+      commentItem.snippet.topLevelComment.snippet.textDisplay += ' (SPAM)';
     }
-  } catch (error) {
-    console.error(error);
   }
 }
 
-// Call the function to start processing comments
-processComments(videoId);
+// Main function to execute the comment processing
+function main() {
+  const videoId = extractVideoIdFromUrl();
+
+  if (videoId) {
+    processComments(videoId);
+  } else {
+    console.error('Invalid video ID in URL');
+  }
+}
+
+// Start processing comments when the page loads
+window.addEventListener('load', main);
